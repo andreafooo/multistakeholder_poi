c0b7a33d70a20b802fc1ba66f42907e98473463a
andreafooo <forster.andrea1997@gmail.com>
2026-05-12 08:08:07 +0200

    set eval to top-10

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 import os
 
 available_datasets = [
-    "foursquaretky",
+    "yelp", "gowalla", "brightkite"
 ]  # choose betweeen "yelp", "gowalla", "foursquaretky", and "brightkite" and make sure to add the datasets to your BASE_DIR
 
 PROJECT_BASE = os.path.dirname(os.path.abspath(__file__))
