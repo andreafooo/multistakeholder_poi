c6c3db57ddfd17f4f3d064fa84b622add1b85543
andreafooo <forster.andrea1997@gmail.com>
2026-05-20 11:17:42 +0200

    adapt config

--- globals.py
+++ globals.py
@@ -1,7 +1,7 @@
 import os
 
 available_datasets = [
-    "yelp"
+    "foursquaretky"
 ]  # choose betweeen "yelp", "gowalla", "foursquaretky", and "brightkite" and make sure to add the datasets to your BASE_DIR
 
 PROJECT_BASE = os.path.dirname(os.path.abspath(__file__))
@@ -15,7 +15,7 @@ models_for_recbole = [
     "BPR"
 ]  # add general recommendation models as baseline (e.g. BPR, SimpleX, ItemKNN, etc.)
 top_k_resample = 150
-top_k_eval = 150
+top_k_eval = 10
 valid_popularity = "item_pop"
 recommendation_dirpart = "recommendations"
 
