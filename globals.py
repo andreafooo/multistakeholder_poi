42898f2077a48e5952818c3d2164685468c52ac9
andreafooo <forster.andrea1997@gmail.com>
2026-06-04 21:53:22 +0200

    remove artefacts, rename re-rankers

--- globals.py
+++ globals.py
@@ -1,8 +1,8 @@
 import os
 
 available_datasets = [
-    "foursquaretky"
-]  # choose betweeen "yelp", "gowalla", "foursquaretky", and "brightkite" and make sure to add the datasets to your BASE_DIR
+    "foursquaretky", "yelp"
+]  # choose betweeen "yelp" and "foursquaretky", and make sure to add the datasets to your BASE_DIR
 
 PROJECT_BASE = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.join(PROJECT_BASE, "datasets") 
@@ -12,7 +12,7 @@ datasets_for_recbole = [
 ]  # add datasets for recbole from above with "_sample" suffix - make sure to add them to recbole_general_recs/dataset
 
 models_for_recbole = [
-    "LightGCN"
+    "BPR"
 ]  # add general recommendation models as baseline (e.g. BPR, SimpleX, ItemKNN, etc.)
 top_k_resample = 150
 top_k_eval = 10
@@ -21,6 +21,5 @@ recommendation_dirpart = "recommendations"
 full_eval_methods = ["baseline", "cp_min_js", "mmr", "geo", "borda", "schulze"]
 boosting_methods = ["borda2baseline", "borda2cp_min_js", "borda2geo", "borda2mmr", 
                     "schulze2baseline", "schulze2cp_min_js", "schulze2geo", "schulze2mmr"]
-
-
+MMR_LAMBDA = 1  # 0 = pure relevance, 1 = pure diversity
 
