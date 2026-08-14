18b80f3ddf328d9c0c0431b114b534f42a3d66ee
andreafooo <forster.andrea1997@gmail.com>
2026-08-14 15:32:25 +0200

    all bash scripts in same dir

--- run_preprocessing.sh
+++ run_preprocessing.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 source venv/bin/activate
 
-# python3 ./preprocessing/data_sampling.py
-# python3 ./preprocessing/metadata_dataset_sample.py
+python3 ./preprocessing/data_sampling.py
+python3 ./preprocessing/metadata_dataset_sample.py
 
 datasets=$(python3 -c "from globals import available_datasets; print(' '.join(available_datasets))")
 for dataset in $datasets; do
